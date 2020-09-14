29ea7430344a378b73c22b2a0eab2d97fc74e70b
stany <2001stany@gmail.com>
2020-09-15 00:03:51 +0300

    script to generate requirements.txt

--- dejango.py
+++ dejango.py
@@ -2,10 +2,10 @@
 import os
 
 # installing the packages required
-# pip install psycopg2-binary gunicorn django-heroku dj-database-url
+pip install psycopg2-binary gunicorn django-heroku dj-database-url
 
 # create a requirements.txt file
-# pip freeze > requirements.txt
+pip freeze > requirements.txt
 
 # create a Procfile
 project_directory = os.getcwd()
@@ -14,8 +14,8 @@ project_name = split_dirs[-1]
 
 # print(project_name)   # code to check if the project_name is correct
 
-
 with open('Procfile', 'x') as f:
     f.write('web: gunicorn ' +project_name+'.wsgi:application')  # make sure the project name is correct
+
 
 print('hello am running')
