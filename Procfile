8a8e341697f11301dba8f106bdd7a1f511747baf
Stanley <2001stany@gmail.com>
2020-12-09 16:13:09 +0300

    add a script to create a Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dejango_tool.wsgi:application
--- dejango.py
+++ dejango.py
@@ -4,26 +4,69 @@ import sys
 import subprocess
 import logging
 
-# installing the packages required
-# logger = logging.getLogger(__name__)
-# logger.info('Installing the required packages...')    # need to color the logging here!
 
-os.system('pip install gunicorn psycopg2-binary django-heroku dj-database-url')
+# pypi needs this instead of print statements
+# change all prints to be logging
+# use the colored logging statements
+
+
+# ask the user to enter the project-name
+project_name = input('Whats your project name:')
+
+   
+try:
+    os.system('pip install gunicorn psycopg2-binary django-heroku dj-database-url')
+    print('DONE: All packages are installed successfully')
+
+except FileExistsError:
+    print('DONE: All packages are installed successfully')
 
 
 # create a requirements.txt file
-os.system('pip freeze > requirements.txt')  # display a logging here, let user know if
-                                            # the installation is complete
+try:
+    os.system('pip freeze > requirements.txt')
+    print('DONE: requirements.txt file created')
+
+except FileExistsError:
+    print('DONE: requirements.txt file created')
+
+
 
 # create a Procfile
-project_directory = os.getcwd()
-split_dirs = project_directory.split('/')
-project_name = split_dirs[-1]
+try:
+    with open('Procfile', 'x') as f:
+        # make sure the project name is correct
+        f.write('web: gunicorn ' + project_name+'.wsgi:application')
 
-# print(project_name)   # code to check if the project_name is correct
+except FileExistsError:
+    print('DONE: Procfile created')
 
-# with open('Procfile', 'x') as f:
-#     f.write('web: gunicorn ' +project_name+'.wsgi:application')  # make sure the project name is correct
+
+# project_directory = os.getcwd()
+# split_dirs = project_directory.split('/')
+# project_folder = split_dirs[-1] 
+
+
+
+# a function to prepend the import statement
+to_settings = os.chdir(project_name)
+
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+try:
+    line_prepender('settings.py', 'import dj_database_url')
+    line_prepender('settings.py', 'import django_heroku')
+
+except FileExistsError:
+    print('DONE: All packages are imported')
+
+
+
+
 
 
 # print('hello am running')
